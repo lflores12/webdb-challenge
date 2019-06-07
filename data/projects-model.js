2c6c41d5b652a501daea664104b55388f7b729fe
Leonel <leonelaf12@gmail.com>
2019-06-07 13:49:07 -0400

    making changes to get project by id

--- data/projects-model.js
+++ data/projects-model.js
@@ -5,7 +5,8 @@ const db = knex(knexConfig.development);
 module.exports = {
     getProjects,
     getProjectById,
-    addProject
+    addProject,
+    getProjectActions
 };
 
 function getProjects() {
@@ -14,10 +15,16 @@ function getProjects() {
 
 function getProjectById(id) {
     return db('projects')
-        .join('actions', 'projects.id', '=', 'actions.project_id')
-        .select('projects.id', 'projects.name', 'projects.description','projects.is_complete' , 'actions.id', {action: 'actions.id'})
+        .join('actions', 'projects.id','actions.project_id',)
+        .select('projects.id', 'projects.name', 'projects.description','projects.is_complete', {action: 'actions.description'})
         .where({ 'projects.id': id })
         
+  }
+  function getProjectActions(projectId) {
+    return db('actions as a')
+      .join('projects as p', 'p.id', 'a.project_id')
+      .select('a.id', 'a.description', 'a.notes', 'a.is_complete')
+      .where('a.project_id', projectId);
   }
 
 function addProject(project) {

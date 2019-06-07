a2ef3af389c24ec654747648c024a3e47a6942bb
Leonel <leonelaf12@gmail.com>
2019-06-07 11:51:02 -0400

    made knexfile and added migrations and both tables added

--- api/server.js
+++ api/server.js
@@ -6,4 +6,8 @@ const server = express();
 server.use(helmet());
 server.use(express.json());
 
+server.get('/', (req, res) => {
+    res.status(200).json({ message: "Hello"})
+});
+
 module.exports = server;

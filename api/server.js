ccc906c43ba20f7e8b8d07203523d5aa1c96d9c3
Leonel <leonelaf12@gmail.com>
2019-06-07 14:09:11 -0400

    made a route for actions

--- api/server.js
+++ api/server.js
@@ -22,14 +22,31 @@ server.get('/api/projects', async (req, res) => {
     }
 });
 
-server.get('/api/projects/:id', async (req, res) => {
-    try {
-        const project = await Projects.getProjectById(req.params.id);
-        res.status(200).json(project)
-    } catch(error) {
-        res.status(500).json({ message: "Error retrieving that project"})
-    }
-});
+// server.get('/api/projects/:id', async (req, res) => {
+//     try {
+//         const project = await Projects.getProjectById(req.params.id);
+//         res.status(200).json(project)
+//     } catch(error) {
+//         res.status(500).json({ message: "Error retrieving that project"})
+//     }
+// });
+
+server.get('/api/projects/:id/actions', (req, res) => {
+    Projects.getProjectById(req.params.id)
+        .then(project => {
+            if (project) {
+                Projects.getProjectActions(req.params.id)
+                    .then(actions => {
+                        res.status(201).json(actions)
+                    })
+                    .catch(err => {
+                        res.status(500).json({ error: err, message: 'Cannot retrieve acitons from database.' })
+                    })
+            } else {
+                res.status(404).json({ errorMessage: 'A project with the specified ID does not exist.' })
+            }
+        })
+})
 
 server.post('/api/projects', async (req, res) => {
     try {
